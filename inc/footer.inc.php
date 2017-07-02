cae73e3f033fda9248c065a8fbd97b3b83daa0a4
guillaumegogo <guillaume.gogo@jeunesse-sports.gouv.fr>
2017-07-02 22:21:58 +0200

    Delete footer.inc.php

--- inc/footer.inc.php
+++
@@ -1,9 +0,0 @@
-<footer>
-	<div class="version"><?php echo $version; ?></div>
-	<ul>
-		<li><a href="../admin/index.php" target="_blank">Administration</a> 
-		<li><a href="mentions.php">Mentions légales</a> 
-		<li><a href="#">Contact</a> 
-		<li>Un service proposé par le <a href="http://jeunes.gouv.fr" target="_blank">Ministère chargé de la Jeunesse</a>.
-	</ul>
-</footer>
