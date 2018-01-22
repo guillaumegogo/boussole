9a5ae08ced2f0af68c05aea5feea3338f30489f6
guillaumegogo <guillaume.gogo@jeunesse-sports.gouv.fr>
2018-01-22 18:01:00 +0100

    marianne

--- www/view/index.tpl.php
+++ www/view/index.tpl.php
@@ -55,6 +55,7 @@
 		<div class="wrapper">
 			<div class="wrapper-bandeau-homepage">
 
+				<img src="img/marianne.png" alt="Ministère de l'éducation nationale" class="logo-ministere-homepage">
 				<a  href="index.php"><img src="img/logo-boussole.svg" alt="logo la boussole des jeunes" class="logo-boussole-homepage"></a>
 
 				<div class="container bonhomme-section-header">
