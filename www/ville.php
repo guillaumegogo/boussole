cbdbb521b7f51bafe54a333b117834bb6b4334bb
guillaumegogo <guillaume.gogo@jeunesse-sports.gouv.fr>
2018-02-16 18:42:31 +0100

    .

--- www/ville.php
+++ www/ville.php
@@ -1,13 +1,12 @@
 <?php
 //ce fichier fait la recherche sur la liste des villes, avec remplacement des caractères pour correspondre au fichier de l'insee. 
-//il ne retourne que 10 résultats pour éviter de faire planter la page d'accueil.
 
-//liste villes
+//récupération de la liste des villes et mise en tableau
 $tmp = file_get_contents('src/villes_index.inc');
 $villes = str_replace('"', '', explode(',', $tmp));
 unset($tmp);
 
-//normalisation
+//normalisation du mot saisi
 $a_remplacer = array ( 'á', 'â', 'ç', 'é', 'è', 'ê', 'ë', 'î', 'ï', 'ö', 'ô', 'ü', 'û', '-', '\'');
 $remplacants = array ( 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'o', 'o', 'u', 'u', ' ', ' ');
 $regex_a_remplacer = '/^saint/';
@@ -17,7 +16,9 @@ $term = (isset($_GET['term'])) ? $_GET['term'] : '';
 $term = str_replace($a_remplacer, $remplacants, $term);
 $term = preg_replace($regex_a_remplacer, $regex_remplacant, $term);
 
-$matches  = preg_grep ('/^('.$term.'.*)|(.* '.$term.')$/i', $villes);
+//on récupère les villes correspondantes au mot saisi (soit il commence par la saisie, soit c'est le code postal)
+$matches  = preg_grep ('/^('.$term.'.*)|(.* '.$term.'\d*)$/i', $villes);
 unset($villes);
 
+//on ne retourne que 10 résultats pour éviter de charger la page d'accueil
 echo json_encode(array_slice($matches,0,10)); 
